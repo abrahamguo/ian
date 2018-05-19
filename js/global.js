a1c8a301e01032ac8c54cc1d5df6d1d507c99916
Abraham Guo <abrahamjguo@gmail.com>
2018-05-19 12:26:13 -0500

    Sending to Ian

---
+++ js/global.js
@@ -0,0 +1,9 @@
+//alert("hi");
+//console.log("howdy");
+var name = prompt("What is your name?");
+var greeting = ["Howdy", "Hi", "Hello", "Whats up", "Aloha"];
+var random = Math.floor(Math.random()* greeting.length);
+alert(greeting[random] + ", " + name);
+function Animate() {
+
+}
